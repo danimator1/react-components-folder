b04fe3c152424bc70f325fc5702333d28c9202f1
DRaymond <danimator1@gmail.com>
2024-06-10 20:36:17 -0400

    created weatherapp

---
+++ react-components-lab/src/App.jsx
@@ -0,0 +1,60 @@
+import './App.css'
+import WeatherForecast from './Components/WeatherForecast'
+
+function App() {
+  const weatherForecasts = [
+    {
+      day: 'Mon',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg',
+      imgAlt: 'sun icon',
+      conditions: 'sunny',
+      time: 'Morning',
+    },
+    {
+      day: 'Tue',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg',
+      imgAlt: 'moon icon',
+      conditions: 'clear',
+      time: 'Night',
+    },
+    {
+      day: 'Wed',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg',
+      imgAlt: 'clouds with lightning icon',
+      conditions: 'stormy',
+      time: 'All Day',
+    },
+    {
+      day: 'Thu',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg',
+      imgAlt: 'sun overcast by clouds icon',
+      conditions: 'overcast',
+      time: 'Evening',
+    },
+    {
+      day: 'Fri',
+      img: 'http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg',
+      imgAlt: 'moon overcast by clouds icon',
+      conditions: 'cloudy',
+      time: 'Night',
+    },
+  ];
+  
+  
+
+  return (
+    <>
+    <h1>Local Weather</h1>
+    <section>
+    <WeatherForecast forecast={weatherForecasts[0]}/>
+    <WeatherForecast forecast={weatherForecasts[1]}/>
+    <WeatherForecast forecast={weatherForecasts[2]}/>
+    <WeatherForecast forecast={weatherForecasts[3]}/>
+    </section>
+  </>
+  
+      
+  )
+}
+
+export default App
---
+++ react-components-lab/src/Components/WeatherForecast.jsx
@@ -0,0 +1,16 @@
+function WeatherForecast (props) {
+  console.log(props)
+
+    return (
+      <div className="weather">
+      <h2>{props.forecast.day}</h2>
+      <img src={props.forecast.img} alt={props.forecast.imgAlt} />
+      <p><span>conditions: </span>{props.forecast.conditions}</p>
+      <p><span>time: </span>{props.forecast.time}</p>
+    </div>
+    
+
+      )
+}
+
+export default WeatherForecast
